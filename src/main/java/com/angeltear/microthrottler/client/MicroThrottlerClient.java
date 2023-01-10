6358856241c3ee0d1ec786fbde683bb23fd8d751
Angeltear <kaloyan.shivarov@gmail.com>
2023-01-10 23:53:59 +0200

    Added RateLimiter, provided by the Guava library. Added comments to explain the functionality and chosen approach.

--- src/main/java/com/angeltear/microthrottler/client/MicroThrottlerClient.java
+++ src/main/java/com/angeltear/microthrottler/client/MicroThrottlerClient.java
@@ -3,6 +3,7 @@ package com.angeltear.microthrottler.client;
 import com.angeltear.microthrottler.Config.RedisConfig;
 import com.angeltear.microthrottler.Model.PaymentRequest;
 import com.angeltear.microthrottler.Serializer.PaymentRequestSerializer;
+import com.google.common.util.concurrent.RateLimiter;
 import io.lettuce.core.KeyValue;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisCommandTimeoutException;
@@ -16,6 +17,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.Date;
 
 @Component
 @Slf4j
@@ -27,6 +29,10 @@ public class MicroThrottlerClient {
     @Value("${redis.connection-timeout-seconds}")
     private long connectionTimeout;
 
+    @Value("${rate-limit-per-second}")
+    private long rateLimit;
+
+
     /* Using @PostConstruct to tell Spring we need the method executed once the context and properties have been initialized.
      *  If the method is called from the main method of the application, it will fail, because context loads later and Spring
      *  can not inject the RedisConfig instance. In any case, PostConstruct works great, because we need to call this method once
@@ -34,6 +40,10 @@ public class MicroThrottlerClient {
      */
     @PostConstruct
     public void initiateConsumer() {
+
+        //Create amount of tokens, generated every second, based on a configuration parameter.
+        RateLimiter rateLimiter = RateLimiter.create(rateLimit);
+
         RedisClient redisClient = redisConfig.getClient();
         log.info("Service started!");
         //Open a connection to the redis client submitting and accepting byte arrays instead of strings, since we're using custom complex objects.
@@ -58,7 +68,13 @@ public class MicroThrottlerClient {
                 log.info("Element obtained: " + pr.toString());
                 //Element is successfully obtained, so remove an attempt from client's personal list of requests.
                 syncCommands.rpop(Long.toString(pr.getClientId()).getBytes());
-                Thread.sleep(2000); //For test purposes
+
+                /* Pass the element to the processor, along with the rateLimiter instance. We can also call the .acquire() method here to block and wait for
+                 * available token before the call to processElement, but passing the limiter instance to get a token inside the method makes this usage easier
+                 * to expand the functionality by passing the processElement to another thread for async processing.
+                */
+                processElement(rateLimiter, pr);
+
             }
             /* When no new elements appear to the queue before the timeout time is reached,
              * close the connection to avoid consuming the whole connection pool and recursively call this method to
@@ -78,6 +94,22 @@ public class MicroThrottlerClient {
             }
 
         }
+    }
+
+    /* In order to throttle the load on the database for a consistent performance, there are 2 algorithms that suit the purpose - Leaky Bucket and Token Bucket.
+     * - Leaky bucket relies on the concept that inbound rate elements is not limited and processed with a fixed frequency. If the volume of inbound elements is too large,
+     * subsequent requests are discarded (bucket is full). Such algorithm is implemented in the initiator Microservice in order to prevent abuse, DDoS, etc.
+     * - Token bucket on the other hand relies on a "token" system that refills over time. This approach limits the average inflow rate (the popping of elements from the queue)
+     * and processes the elements if there's enough tokens available in the bucket. This allows for a sudden spike in traffic. Several tokens can be obtained at the same time, but
+     * if the max amount of tokens is reached, subsequent requests need to wait for a token to be released (processing of previous element has finished). This ensures steady average
+     * flow to the database. Token Bucket is the approach that is selected for throttling the DB requests in this Microservice.
+     *
+     * For implementing the throttling approach, we're using Google's "Guava" open-source library. It generates tokens at a fixed rate (with or without warm-up period), has both
+     * blocking (acquire) and non-blocking (tryAcquire) functionality. Its usage is also straight-forward.
+     */
+    public void processElement(RateLimiter rateLimiter, PaymentRequest paymentRequest){
+        rateLimiter.acquire();
+        log.info("Processing paymentRequest: " + paymentRequest.toString() + " at: " + new Date());
     }
 
 
